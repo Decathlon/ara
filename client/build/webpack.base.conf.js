b69c16a4b38effc21f0484d3c39d5346d15761db
Sylvain Nieuwlandt <external.z10snieu@decathlon.com>
2019-09-11 16:27:35 +0200

    Fix js-yaml vulnerability

--- client/build/webpack.base.conf.js
+++ client/build/webpack.base.conf.js
@@ -2,6 +2,7 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -11,6 +12,9 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
