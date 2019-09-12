a1452058e60de52fe16024857fb1f0bcb29f834a
Sylvain NIEUWLANDT <45944760+dktsni@users.noreply.github.com>
2019-09-12 15:51:46 +0200

    chore: fix remaining npm securities vulnerabilities (#156)
    * Fix set-value vulnerability
    * Fix lodash.defaultsdeep & lodash.mergewith vulnerabilities
    * Fix braces vulnerability
    * Fix js-yaml vulnerability
    * Upgrade to babel v7
    * Upgrade css-loader, eslint & webpack
    * Upgrade at max the lodash dependencies

--- client/build/utils.js
+++ client/build/utils.js
@@ -1,6 +1,6 @@
 var path = require('path')
 var config = require('../config')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 exports.assetsPath = function (_path) {
   var assetsSubDirectory = process.env.NODE_ENV === 'production'
@@ -15,7 +15,6 @@ exports.cssLoaders = function (options) {
   var cssLoader = {
     loader: 'css-loader',
     options: {
-      minimize: process.env.NODE_ENV === 'production',
       sourceMap: options.sourceMap
     }
   }
@@ -35,10 +34,7 @@ exports.cssLoaders = function (options) {
     // Extract CSS when that option is specified
     // (which is the case during production build)
     if (options.extract) {
-      return ExtractTextPlugin.extract({
-        use: loaders,
-        fallback: 'vue-style-loader'
-      })
+      return [MiniCssExtractPlugin.loader].concat(loaders)
     } else {
       return ['vue-style-loader'].concat(loaders)
     }
