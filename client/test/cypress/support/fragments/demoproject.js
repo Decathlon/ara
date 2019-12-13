62bb8df6727d59356acb6c96080c8a56cf6c0bce
Sylvain Nieuwlandt <external.z10snieu@decathlon.com>
2019-12-13 16:52:38 +0100

    Add a link to a GitHub issue for the todo

--- client/test/cypress/support/fragments/demoproject.js
+++ client/test/cypress/support/fragments/demoproject.js
@@ -2,8 +2,8 @@ export const reset = () => {
   cy.get('body').then(body => {
     if (body.find('[data-nrt="deleteDemo"]').length >  0) {
       cy.GetByDataNrt('deleteDemo').click()
-      // TODO : Replace the basic Modal component of iviewui by a custom one to have
-      // the posibitlity to add custom attributes (like a data-nrt).
+      // TODO(Issue224) : Replace the basic Modal component of iviewui by a custom one
+      // to have the posibitlity to add custom attributes (like a data-nrt).
       cy.get('.ivu-modal-confirm-footer').children().next().click()
     }
   });
