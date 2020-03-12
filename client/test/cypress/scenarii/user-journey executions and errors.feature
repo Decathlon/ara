3b544f0ca728083b0d77fadaba43e0f873be3bb0
Caroline Lavergne <caroline.lavergne.partner@decathlon.com>
2020-03-12 15:59:17 +0100

    test: update data-nrt, feature and mock api response

--- client/test/cypress/scenarii/user-journey executions and errors.feature
+++
@@ -1,7 +0,0 @@
-Feature: Journey
-
-@severity-sanity-check
-  Scenario: Check the exeuction cart
-  Given the user goes to the home page
-    And on the executions and errors page, the user clicks on the actions and job reports button "48"
-    Then on the executions and errors page, in the actions and job reports list, the actions button "48" is visible
--- client/test/cypress/support/fragments/executions.js
+++ client/test/cypress/support/fragments/executions.js
@@ -1,10 +1,27 @@
 export const url = '/projects/the-demo-project/executions';
 
   
-export const getActionsAndJobReportsButton = (id) => {
-    return cy.GetByDataNrt('ExecutionsAndErrorsActionsAndJobReportsButton_' + id);
+export const getActionsAndJobReportsButton = (executionId) => {
+    return cy.GetByDataNrt('executions_ActionsAndJobReportsButton_' + executionId);
 }
 
-export const getActionsButton = (id) => {
-    return cy.GetByDataNrt('ExecutionsAndErrorsActionsAndJobReportsButton_Actions_' + id)
+export const getButton = (label, executionId) => {
+    return cy.GetByDataNrt('executions_ActionsAndJobReportsButton_' + label + '_' + executionId)
+}
+
+export const getRun = (runId, executionId) => {
+    return cy.GetByDataNrt('executions_CartRowTitle_' + runId + '_' + executionId);
+}
+
+export const getCartRowSubTitle = (runId, teamId, executionId) => {
+    return cy.GetByDataNrt('executions_CartRowSubTitle_' + runId + '_' + teamId + '_' + executionId);
+}
+export const getCartHeader = (qualityId, executionId) => {
+    return cy.GetByDataNrt('executions_CartHeader_' + qualityId + '_' + executionId);
+
+
+}
+
+export const getCartRowHeader = (qualityId, runId, executionId) => {
+    return cy.GetByDataNrt('executions_CartRowHeader_' + qualityId + '_' + runId + '_' + executionId);
 }
