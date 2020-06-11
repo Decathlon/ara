30a0d0003e23e87a022c5f19d06033da9ed391f4
Omar CHAHBOUNI <omar.chahbouni.partner@decathlon.com>
2020-06-11 10:03:08 +0200

    doc: write documentation about adding a new test framework

--- server/src/main/java/com/decathlon/ara/scenario/common/indexer/ScenariosIndexer.java
+++ server/src/main/java/com/decathlon/ara/scenario/common/indexer/ScenariosIndexer.java
@@ -8,5 +8,12 @@ import java.util.List;
 
 public interface ScenariosIndexer {
 
+    /**
+     * Create the executed tests scenarios
+     * @param parentFolder the run folder containing all the files needed to create the {@link List} of {@link ExecutedScenario}
+     *                     For instance the folder api for POSTMAN or the folder firefox-desktop for CUCUMBER
+     * @param run the {@link Run} for which the {@link List} of {@link ExecutedScenario} are requested.
+     * @return the executed tests scenarios
+     */
     List<ExecutedScenario> getExecutedScenarios(File parentFolder, Run run);
 }
