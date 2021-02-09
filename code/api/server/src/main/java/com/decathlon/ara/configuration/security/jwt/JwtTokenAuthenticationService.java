ec6a8fe6ef7432fa787bceaa93513f55ee605e8d
Omar CHAHBOUNI <omar.chahbouni.partner@decathlon.com>
2021-02-09 12:07:46 +0100

    refactor: update JWT authentication class

--- code/api/server/src/main/java/com/decathlon/ara/configuration/security/jwt/JwtTokenAuthenticationService.java
+++ code/api/server/src/main/java/com/decathlon/ara/configuration/security/jwt/JwtTokenAuthenticationService.java
@@ -53,7 +53,7 @@ public class JwtTokenAuthenticationService {
     @NonNull
     private final AuthenticationJwtTokenConfiguration tokenConfiguration;
 
-    private static final String tokenCookieName = "ara-access-token";
+    private static final String TOKEN_COOKIE_NAME = "ara-access-token";
 
     /**
      * Create a header containing an authentication cookie, if the authentication is enabled
@@ -88,7 +88,7 @@ public class JwtTokenAuthenticationService {
             cookieValue = cookieValueAndAge.getFirst();
             cookieAge = cookieValueAndAge.getSecond();
         }
-        HttpCookie cookie = ResponseCookie.from(tokenCookieName, cookieValue)
+        HttpCookie cookie = ResponseCookie.from(TOKEN_COOKIE_NAME, cookieValue)
                 .maxAge(cookieAge)
                 .httpOnly(true)
                 .secure(tokenConfiguration.isUsingHttps())
@@ -187,7 +187,7 @@ public class JwtTokenAuthenticationService {
         }
         Cookie[] cookies = request.getCookies() != null ? request.getCookies() : new Cookie[0];
         return Arrays.stream(cookies)
-                .filter(cookie -> tokenCookieName.equals(cookie.getName()))
+                .filter(cookie -> TOKEN_COOKIE_NAME.equals(cookie.getName()))
                 .map(Cookie::getValue)
                 .map(JwtAuthentication::new)
                 .map(Authentication.class::cast)
@@ -246,7 +246,7 @@ public class JwtTokenAuthenticationService {
 
         @Override
         public void setAuthenticated(boolean b) throws UnsupportedOperationException {
-
+            // custom class: no need to set authenticated field
         }
 
         @Override
