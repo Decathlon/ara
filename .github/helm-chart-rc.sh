a5d595c29ef6761acbd19ec2115597b6acd9d296
Thomas GRUSON <thomas.gruson@protonmail.com>
2021-03-01 17:31:55 +0100

    fix: ci

--- .github/helm-chart-rc.sh
+++ .github/helm-chart-rc.sh
@@ -3,8 +3,12 @@
 STABLE_CHART=$(cat charts/stable/Chart.yaml | grep version | sed -e 's/version: //')
 STABLE_CHART_BASE=$(echo "${STABLE_CHART}" | sed -e 's/-rc.*//')
 
-STABLE_CHART_NUMBER=$(echo "${STABLE_CHART}" | sed -e 's/.*-rc\.//')
-STABLE_CHART_NUMBER=$((STABLE_CHART_NUMBER+1))
+if [[ $STABLE_CHART == *-rc* ]]; then
+  STABLE_CHART_NUMBER=$(echo "${STABLE_CHART}" | sed -e 's/.*-rc\.//')
+  STABLE_CHART_NUMBER=$((STABLE_CHART_NUMBER+1))
+else
+  STABLE_CHART_NUMBER=0
+fi
 STABLE_CHART="${STABLE_CHART_BASE}-rc.${STABLE_CHART_NUMBER}"
 
 sed -i'.old' "s/^version:.*$/version: ${STABLE_CHART}/" charts/stable/Chart.yaml
