c10f42684c29819fef4a66707f05caf9dbd217d0
Thomas GRUSON <thomas.gruson@protonmail.com>
2021-03-04 17:26:45 +0100

    feat: optimize CI (#409)
    * feat: one sonar analysis
    
    * feat: add candidate chart
    
    * chore: chart release ci
    
    * chore: build ci
    
    * feat: sonar analysis
    
    * Remove useless workflow
    
    * feat: build rc from pr
    
    * fix: docker login condition
    
    * fix: sonar scan

--- .github/helm-chart-stable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-STABLE_CHART=$(cat charts/stable/Chart.yaml | grep version | sed -e 's/version: //')
-STABLE_CHART_BASE=$(echo "${DEV_CHART}" | sed -e 's/-rc.*//')
-
-sed -i'.old' "s/^version:.*$/version: ${STABLE_CHART_BASE}/" charts/stable/Chart.yaml
