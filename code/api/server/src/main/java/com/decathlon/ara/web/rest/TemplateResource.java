d167721340913bc76bdf07de97f38d2c01c45b88
Thomas GRUSON <thomas.gruson@protonmail.com>
2021-05-21 09:32:52 +0200

    feat: add template execution (#423)
    * feat: add template execution html
    
    * chore: update api version to 8.2.0
    
    * chore: upgrade docker postgresql to version 12
    
    * fix: make stop-X
    
    * feat: add docker shell node for web-ui
    
    * fix: exclude test classes from sonar analysis
    
    * chore: upgrade candidate chart
    
    * feat: use project code instead of project id
    
    * fix: sonar bug
    
    * fix: duplication
    
    * chore: add tests on TemplateResource
    
    * fix: sonar analysis
    
    * chore: add unit tests
    
    * chore: apply review

---
+++ code/api/server/src/main/java/com/decathlon/ara/web/rest/TemplateResource.java
@@ -0,0 +1,47 @@
+package com.decathlon.ara.web.rest;
+
+import com.decathlon.ara.Entities;
+import com.decathlon.ara.service.ExecutionHistoryService;
+import com.decathlon.ara.service.ProjectService;
+import com.decathlon.ara.service.exception.NotFoundException;
+import com.decathlon.ara.web.rest.util.RestConstants;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.transaction.Transactional;
+
+@Controller
+@RequestMapping(TemplateResource.PATH)
+@RequiredArgsConstructor
+@Transactional(Transactional.TxType.REQUIRED)
+public class TemplateResource {
+    private static final String NAME = "template";
+    /**
+     * The full path to this Rest resource from the basename.
+     */
+    static final String PATH = RestConstants.API_PATH + "/" + NAME + "s";
+
+    private final ExecutionHistoryService executionHistoryService;
+    private final ProjectService projectService;
+
+    @GetMapping("cycle-execution")
+    public String nrtCycle(@RequestParam("project") String projectCode,
+                           @RequestParam String branch,
+                           @RequestParam String cycle,
+                           Model model) throws NotFoundException {
+        var projectId = projectService.toId(projectCode);
+        var latestExecutions = executionHistoryService.getLatestExecutionHistories(projectId);
+        var execution = latestExecutions.stream()
+                .filter(e -> branch.equals(e.getBranch()))
+                .filter(e -> cycle.equals(e.getName()))
+                .findFirst()
+                .orElseThrow(() -> new NotFoundException("The branch and cycle couple for this project does not exists: it has perhaps been removed.", Entities.CYCLE_DEFINITION));
+        model.addAttribute("execution", execution);
+        return "cycle-execution";
+    }
+
+}
