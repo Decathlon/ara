f3a0df70ffcbf230ac9e6ed056080e2b23355fe2
Thomas GRUSON <thomgrus@gmail.com>
2021-08-30 11:29:15 +0200

    feat: add core-api

---
+++ code/core-api/src/test/java/com/decathlon/ara/coreapi/CoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decathlon.ara.coreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
