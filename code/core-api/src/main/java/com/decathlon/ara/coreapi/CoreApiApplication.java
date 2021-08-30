5942bf36f6fe52cc236b43e9c9cfd2f9e84819df
Thomas GRUSON <thomgrus@gmail.com>
2021-08-30 15:20:03 +0200

    feat: add core-api (#487)
    * feat: add core-api
    
    * feat: add coverage

---
+++ code/core-api/src/main/java/com/decathlon/ara/coreapi/CoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.decathlon.ara.coreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreApiApplication.class, args);
+    }
+
+}
