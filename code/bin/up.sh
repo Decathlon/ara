3806c9713889c1591ebab42088360804edf6185a
Thomas GRUSON <thomgrus@gmail.com>
2021-10-13 11:45:28 +0200

    feat: change yml to yaml

--- code/bin/up.sh
+++ code/bin/up.sh
@@ -5,6 +5,6 @@ BIN_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${BIN_PATH}/..
 # Stop previous version except if asked ( NO_DOWN var set )
-test -z "$NO_DOWN" && docker-compose -f docker-compose.dev.yml down --remove-orphans
-docker-compose -f docker-compose.dev.yml up -d $@
+test -z "$NO_DOWN" && docker-compose -f docker-compose.dev.yaml down --remove-orphans
+docker-compose -f docker-compose.dev.yaml up -d $@
 cd -
