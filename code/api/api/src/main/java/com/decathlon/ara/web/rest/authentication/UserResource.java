15ca9abc716b28f299871f0fc96f30ec48e56b98
regislegrand <regis.legrand.partner@decathlon.com>
2021-10-27 16:09:05 +0200

    fix: remove useless code
    fix: remove useless code

--- code/api/api/src/main/java/com/decathlon/ara/web/rest/authentication/UserResource.java
+++ code/api/api/src/main/java/com/decathlon/ara/web/rest/authentication/UserResource.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +24,6 @@ public class UserResource {
 
     @GetMapping("/details")
     public AuthenticationUserDetailsDTO getUserDetails(
-            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
             @AuthenticationPrincipal OidcUser user
     ){
 
