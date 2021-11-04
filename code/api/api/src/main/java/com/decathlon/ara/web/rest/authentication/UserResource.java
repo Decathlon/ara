c3b42eae487c730abf23edc07cd459622b05605d
Thomas GRUSON <thomgrus@gmail.com>
2021-11-04 10:53:39 +0100

    feat: profile from dev server (#529)
    * feat: profile from dev server
    
    * fix: sonar
    
    Co-authored-by: Omar CHAHBOUNI <47318765+omar-chahbouni-decathlon@users.noreply.github.com>

--- code/api/api/src/main/java/com/decathlon/ara/web/rest/authentication/UserResource.java
+++ code/api/api/src/main/java/com/decathlon/ara/web/rest/authentication/UserResource.java
@@ -3,11 +3,7 @@ package com.decathlon.ara.web.rest.authentication;
 
 import com.decathlon.ara.service.dto.authentication.AuthenticationUserDetailsDTO;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,31 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static com.decathlon.ara.web.rest.util.RestConstants.API_PATH;
 
-@Slf4j
 @RestController
 @RequestMapping(UserResource.PATH)
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor
 public class UserResource {
 
     static final String PATH = API_PATH + "/user";
 
-
     @GetMapping("/details")
     public AuthenticationUserDetailsDTO getUserDetails(
-            @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
             @AuthenticationPrincipal OidcUser user
     ){
 
         return new AuthenticationUserDetailsDTO(
-                user.getClaimAsString("sub"),
+                user.getSubject(),
                 user.getFullName(),
                 user.getName(),
                 user.getEmail(),
                 user.getPicture());
     }
-
-
-
-
 
 }
--- code/oauth2-dev-server/src/main/java/com/decathlon/ara/oauth2devserver/config/DefaultSecurityConfig.java
+++ code/oauth2-dev-server/src/main/java/com/decathlon/ara/oauth2devserver/config/DefaultSecurityConfig.java
@@ -1,24 +1,33 @@
 package com.decathlon.ara.oauth2devserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.oauth2.core.oidc.endpoint.OidcParameterNames;
+import org.springframework.security.oauth2.server.authorization.JwtEncodingContext;
+import org.springframework.security.oauth2.server.authorization.OAuth2TokenCustomizer;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import org.springframework.beans.factory.annotation.Value;
+
 @EnableWebSecurity
 public class DefaultSecurityConfig {
 
+    @Value("${provider.url}")
+    private String providerUrl;
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/oauth2/**", "/actuator/**").permitAll()
-                .anyRequest().authenticated().and()
-                .formLogin(withDefaults());
+        http.authorizeRequests().antMatchers("/oauth2/**", "/user/**", "/actuator/**").permitAll().anyRequest().authenticated()
+                .and().formLogin(withDefaults());
         return http.build();
     }
 
@@ -26,20 +35,27 @@ public class DefaultSecurityConfig {
     public UserDetailsService userDetailsService() {
         var uds = new InMemoryUserDetailsManager();
 
-        var user = User.withDefaultPasswordEncoder()
-                                    .username("user")
-                                    .password("user")
-                                    .roles("USER").build();
+        var user = User.withDefaultPasswordEncoder().username("user").password("user").roles("USER").build();
 
-        var admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("admin")
-                .roles("ADMIN").build();
+        var admin = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build();
 
         uds.createUser(user);
         uds.createUser(admin);
 
         return uds;
+    }
+
+    @Bean
+    OAuth2TokenCustomizer<JwtEncodingContext> jwtCustomizer() {
+        return context -> {
+            if (context.getTokenType().getValue().equals(OidcParameterNames.ID_TOKEN)) {
+                Authentication principal = context.getPrincipal();
+                context.getClaims().claim(StandardClaimNames.PICTURE,
+                        "%s/user/%s.png".formatted(providerUrl, principal.getName()));
+                context.getClaims().claim(StandardClaimNames.EMAIL,
+                        "%s@oauth2-dev-server.localhost".formatted(principal.getName()));
+            }
+        };
     }
 
 }
