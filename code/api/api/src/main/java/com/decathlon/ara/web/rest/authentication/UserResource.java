19e7934aad7b08e399089411d199d00fb18c1d91
Thomas GRUSON <thomgrus@gmail.com>
2021-11-22 08:52:29 +0100

    fix: oauth2-user (#546)
    * fix: oauth2-user
    
    * feat: add TU

--- code/api/api/src/main/java/com/decathlon/ara/web/rest/authentication/UserResource.java
+++ code/api/api/src/main/java/com/decathlon/ara/web/rest/authentication/UserResource.java
@@ -1,15 +1,20 @@
 package com.decathlon.ara.web.rest.authentication;
 
 
+import static com.decathlon.ara.web.rest.util.RestConstants.API_PATH;
+
+import java.util.Optional;
+
 import com.decathlon.ara.service.dto.authentication.AuthenticationUserDetailsDTO;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static com.decathlon.ara.web.rest.util.RestConstants.API_PATH;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(UserResource.PATH)
@@ -20,15 +25,31 @@ public class UserResource {
 
     @GetMapping("/details")
     public AuthenticationUserDetailsDTO getUserDetails(
-            @AuthenticationPrincipal OidcUser user
+            @AuthenticationPrincipal OidcUser oidcUser,
+            @AuthenticationPrincipal OAuth2User oauth2User
     ){
+        if (oidcUser != null) {
+            return new AuthenticationUserDetailsDTO(
+                oidcUser.getSubject(),
+                oidcUser.getFullName(),
+                oidcUser.getName(),
+                oidcUser.getEmail(),
+                oidcUser.getPicture());
+        }
 
-        return new AuthenticationUserDetailsDTO(
-                user.getSubject(),
-                user.getFullName(),
-                user.getName(),
-                user.getEmail(),
-                user.getPicture());
+        if (oauth2User != null) {
+            return new AuthenticationUserDetailsDTO(
+                safeStringAttribute(oauth2User, "id"),
+                safeStringAttribute(oauth2User, "name"),
+                safeStringAttribute(oauth2User, "login"),
+                safeStringAttribute(oauth2User, "email"),
+                safeStringAttribute(oauth2User, "avatar_url"));
+        }
+        return new AuthenticationUserDetailsDTO();
+    }
+
+    private String safeStringAttribute(OAuth2User oauth2User, String attribute) {
+        return Optional.ofNullable(oauth2User.getAttribute(attribute)).orElse("").toString();
     }
 
 }
