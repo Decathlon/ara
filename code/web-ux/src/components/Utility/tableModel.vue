ba0a47134761214d7e6b24da3b2b4613acc7e2b4
Vincent59 <vincent.pierin.partner@decathlon.com>
2023-01-23 14:36:05 +0100

    feat(web-ux): Fix features issue on expand

--- code/web-ux/src/components/Utility/tableModel.vue
+++ code/web-ux/src/components/Utility/tableModel.vue
@@ -6,13 +6,16 @@ const featuresList = reactive([
   {
     name: "Feature 1 children 1",
     type: "folder",
+    parent: true,
+    opened: 'false',
     id: 1,
     children: [
       {
         name: "Feature 1 children 2",
         type: "folder",
+        opened: false,
         id: 2,
-        parentId: 2,
+        parentId: 1,
         children: [
           {
             name: "Feature 1 children 3",
@@ -30,17 +33,21 @@ const featuresList = reactive([
   {
     name: "Feature 2 children 1",
     type: "folder",
+    opened: false,
     id: 4,
+    parent: true,
     children: [
       {
         name: "Feature 2 children 2",
         type: "folder",
+        opened: false,
         id: 5,
         parentId: 4,
         children: [
           {
             name: "Feature 2 children 3",
             type: "folder",
+            opened: false,
             id: 6,
             parentId: 5,
             children: [
@@ -93,11 +100,12 @@ const openFeature = (feature) => {
   if (duplicate) {
     return false;
   } else {
+    feature.opened = true;
     return featuresList.splice(
       index + 1,
       0,
       feature.children[0] ? feature.children[0] : feature.children
-    );
+    )
   }
 };
 </script>
@@ -124,7 +132,7 @@ const openFeature = (feature) => {
                 class="feature-team-select"
                 :identifier="'team-select'"
                 :options="teams"
-              ></VtmnSelect>
+              />
             </span>
           </th>
           <th>
@@ -136,7 +144,7 @@ const openFeature = (feature) => {
                 style="width: 90px"
                 :identifier="'priority-select'"
                 :options="priority"
-              ></VtmnSelect>
+              />
             </span>
           </th>
           <th>
@@ -146,16 +154,16 @@ const openFeature = (feature) => {
                 style="width: 280px"
                 :identifier="'coverage-select'"
                 :options="coverage"
-              ></VtmnSelect>
+              />
             </span>
           </th>
           <th>
-            <span class="vtmn-flex vtmn-justify-evenly vtmn-float-left">
+            <span class="vtmn-flex vtmn-items-center vtmn-justify-evenly vtmn-float-left">
               <VtmnIcon
                 value="settings-line"
                 class="vtmn-mx-4 vtmn-cursor-pointer"
                 :size="24"
-              ></VtmnIcon>
+              />
               <VtmnCheckbox :identifier="'feature-select'" />
             </span>
           </th>
@@ -166,21 +174,24 @@ const openFeature = (feature) => {
           v-for="features in featuresList"
           :key="features.id"
           class="vtmn-m-4"
-          :class="features.type === 'folder' ? 'vtmn-w-full' : 'vtmn-border'"
+          :class="features.type === 'folder' ? 'vtmn-w-full' : 'feature'"
         >
           <td
             v-if="features.type === 'folder'"
             class="feature-name vtmn-flex vtmn-items-center vtmn-typo_text-3"
+            :class="!features.parent ? 'vtmn-pl-5 key' : 'vtmn-flex'"
           >
             <span
               v-if="features.children"
+              class="vtmn-flex"
               @click="openFeature(features, features.children)"
             >
               <VtmnIcon
                 value="play-fill"
-                class="vtmn-m-auto vtmn-cursor-pointer"
-                :size="24"
-              ></VtmnIcon>
+                class="vtmn-mx-2 vtmn-cursor-pointer"
+                :class="features.opened === true ? 'feature-opened' : 'feature-closed'"
+                :size="18"
+              />
             </span>
             {{ features.name }}
           </td>
@@ -190,24 +201,24 @@ const openFeature = (feature) => {
           >
             {{ features.name }}
           </td>
-          <td v-else></td>
+          <td v-else />
           <td
-            class="vtmn-text-center vtmn-typo_text-3"
             v-if="features.type === 'feature'"
+            class="vtmn-text-center vtmn-typo_text-3"
           >
             {{ features.team }}
           </td>
-          <td v-else></td>
+          <td v-else />
           <td
-            class="vtmn-text-center vtmn-typo_text-3"
             v-if="features.type === 'feature'"
+            class="vtmn-text-center vtmn-typo_text-3"
           >
             {{ features.priority }}
           </td>
-          <td v-else></td>
+          <td v-else />
           <td
-            class="vtmn-text-center vtmn-typo_text-3"
             v-if="features.type === 'feature'"
+            class="vtmn-text-center vtmn-typo_text-3"
           >
             {{ features.coverage }}
           </td>
@@ -217,7 +228,7 @@ const openFeature = (feature) => {
                 value="settings-line"
                 class="vtmn-mx-4 vtmn-cursor-pointer"
                 :size="24"
-              ></VtmnIcon>
+              />
               <VtmnCheckbox :identifier="'feature-select'" />
             </span>
           </td>
@@ -242,5 +253,14 @@ const openFeature = (feature) => {
 
 .feature-name {
   color: var(--vtmn-semantic-color_border-inactive);
+}
+
+.feature-opened {
+  transform: rotate(90deg);
+  transition: .3s;
+}
+
+.feature {
+  border: 1px solid #cccccc;
 }
 </style>
