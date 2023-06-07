85efa9ac982d0cac7ca57591684320c485043428
Omar CHAHBOUNI <omar.chahbouni.partner@decathlon.com>
2023-06-07 17:54:52 +0200

    chore: remove core-api and web-ux modules
    BREAKING CHANGE: Remove unsupported modules

--- code/web-ux/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./src/**/*.{html,js,vue}"], // Make sure 'vue' is added here since this is a Vue project
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
-  presets: [require("@vtmn/css-tailwind-preset")], //TO USE VITAMIN WITH TAILWIND
-};
