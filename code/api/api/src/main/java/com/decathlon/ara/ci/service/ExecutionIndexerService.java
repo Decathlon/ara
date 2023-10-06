05b86b224197b94292de6fb2fd19b92c6e15748a
Luigi <luigi.palmiero.partner@decathlon.com>
2023-10-06 10:40:09 +0200

    fix: :ambulance: unsuported operation on list

--- code/api/api/src/main/java/com/decathlon/ara/ci/service/ExecutionIndexerService.java
+++ code/api/api/src/main/java/com/decathlon/ara/ci/service/ExecutionIndexerService.java
@@ -34,10 +34,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -116,7 +113,8 @@ public class ExecutionIndexerService {
 
         final Execution savedExecution = executionRepository.save(processedExecution.get());
 
-        List<Long> newErrorIds = getErrorIds(Optional.of(savedExecution));
+        List<Long> newErrorIds = new ArrayList<>(getErrorIds(Optional.of(savedExecution)));
+
         newErrorIds.removeAll(existingErrorIds);
         if (!newErrorIds.isEmpty()) {
             final Set<Problem> updatedProblems = errorService.autoAssignProblemsToNewErrors(projectId, newErrorIds);
